a384d2b0e1e673d30d726209d8e6d73d47533e45
Phu Dinh <you@>
2020-11-14 14:06:42 +0700

    thêm các hướng dẫn tiếng việt để anh xem

--- NhanDienHinhAnh.py
+++ NhanDienHinhAnh.py
@@ -2,17 +2,24 @@ import cv2
 import os
 
 orb = cv2.ORB_create(nfeatures=1000)
-path = "test/bienBao3"
+path = "test/chiDan"
+#tên đường dẫn đến file cần nhận diện
+#t chia thành 3 thư muc cho máy chạy ổn định
+#một thư mục chỉ dẩn, 1 thư mục cấm
+#và 1 thư mục chỉ dẫn,1 thư mục nguy hiểm
 listAnh = []
+#chứa các data ảnh để nhận diện
 listNameAnh = []
+#chứa các tên ảnh trong thư mục
 myList = os.listdir(path)
+#đọc thư mục
 
 for name in myList:
     imgNow = cv2.imread(f'{path}/{name}', 0)
     listAnh.append(imgNow)
     listNameAnh.append(os.path.splitext(name)[0])
 print(listNameAnh)
-
+#đọc các data file ảnh trong thư mục
 
 def timDacDiem(listAnh):
     NhanDang = []
@@ -44,15 +51,24 @@ def timID(live, NhanDang, phucTap):
 
 
 Nhandang = timDacDiem(listAnh)
+#tìm đặc điểm
 print(len(Nhandang))
+# in số lương ra console
 quay = cv2.VideoCapture(0)
+#mở webcam
 while True:
+#tạo vòng lặp để kiểm tra và nhận diện biển báo
     success, live = quay.read()
     liveMauSac = live.copy()
     live = cv2.cvtColor(live, cv2.COLOR_BGR2GRAY)
 
     tenCuaAnh = timID(live, Nhandang, 6)
+    #nếu đạt những điều kiện t đưa ra thì nó
+    # sẽ in text tên ảnh lên màn hình
     if id != -1:
         cv2.putText(liveMauSac, listNameAnh[tenCuaAnh], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
     cv2.imshow("Phan Mem Nhan Dien Bien Bao", liveMauSac)
-    cv2.waitKey(1)
+    # in lên màn hình
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    # bấm q để tắt kết thúc chương trình
