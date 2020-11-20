d91796d66da4ffead6752c8446ef9b5101736a8c
Phu Dinh <you@>
2020-11-20 17:44:27 +0700

    done

--- NhanDienGiongNoi.py
+++ NhanDienGiongNoi.py
@@ -2,16 +2,17 @@ import speech_recognition as sr
 from gtts import gTTS
 import playsound
 import os
-
+import pyautogui
 
 def switchAnswer(argument):
     switcher = {
-        "đáp án a": 1,
-        "đáp án b": 2,
-        "đáp án c": 3,
-        "đáp án cuối": 4
+        "đáp án a": [670,504],
+        "đáp án b": [1237, 507],
+        "đáp án c": [667,581],
+        "đáp án cuối": [1232, 578],
+        "kết thúc": [5,5]
     }
-    return switcher.get(argument.lower(), "Null")
+    return switcher.get(argument.lower(), [69,69])
     # lower text to compare
 
 
@@ -37,11 +38,17 @@ def get_audio():
             print("Phú không thể nghe thấy gì ")
             return 0
 
+while True:
+    temp = get_audio()
+    temp1 = "ý của bạn là {0}".format(temp)
+    temp = format(temp)
+    print(switchAnswer(temp))
+    if switchAnswer(temp) != [69, 69]:
+        if switchAnswer(temp) == [5, 5]:
+            break
+        pyautogui.click(switchAnswer(temp))
 
-temp = get_audio()
-temp1 = "ý của bạn là {0}".format(temp)
-temp = format(temp)
 
-print(switchAnswer(temp))
+
 # speak(temp)
 # speak("tôi sẽ bấm chuột giúp bạn?")
